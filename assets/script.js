c3af633a29f876b812271a4e200140ed6e3736c3
Giosialu <Giosi.Alessandro@hotmail.it>
2017-07-10 13:37:58 +0200

    Added node graph visualization.

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,115 @@
+//Variabili principali
 var day = "Friday";
+var chart = "node";
+var data;
 
-function updateData() {
-	return;
+//Variabili per la dimensione dell'SVG
+var svgContainer;
+var chartWidth;
+var chartHeight;
+
+//Variabili per la visualizzazione dei nodi
+var maxLinks = 10000;
+var maxNodes = 250;
+
+function createNodeChart() {
+	
+	nv.addGraph(function() {
+			
+		var nodeChart = nv.models.forceDirectedGraph()
+			//Definizione della grandezza del grafico
+			.width(chartWidth)
+			.height(chartHeight)
+			.nodeExtras(function(nodes) {
+				nodes.selectAll("circle")
+					//Definizione del raggio dei nodi
+					.attr("r", function(d) {
+						return (d.inValue + d.outValue) / 1000;
+					})
+					//Definizione del colore dei nodi
+					.attr("style", function(d) {
+						var larger = Math.max(d.inValue, d.outValue);
+						var r, g;
+						if (d.inValue == larger) {
+							r = 255;
+							g = (d.outValue * 255) / larger;
+						}
+						else {
+							g = 255;
+							r = (d.inValue * 255) / larger;
+						}
+						return "fill: rgb(" + Math.round(r) + "," + Math.round(g) + ",0)";
+					});
+				
+			});
+		
+		nodeChart.tooltip.contentGenerator(function(d) {
+			return '<h4>ID: ' + d.id + '</h4>\n<p>Comunicazioni in uscita: ' + d.outValue + '</p>\n<p>Comunicazioni in entrata: ' + d.inValue + '</p>';
+		});
+		
+		d3.select("svg")
+			.datum(data)
+			.call(nodeChart);
+		  
+		return nodeChart;
+		
+    });
+
+}
+
+function updateSizes() {
+	chartWidth = svgContainer.clientWidth;
+	chartHeight = svgContainer.clientHeight;
+}
+
+function updatePage() {
+	
+	$("#loader").fadeIn();
+	
+	updateSizes();
+	
+	switch (chart) {
+		
+		case "node":
+		$.get("nodeRequest", {day: day, maxLinks: maxLinks, maxNodes: maxNodes}, function(result) {
+			data = JSON.parse(result);
+			createNodeChart();
+			$("#loader").fadeOut();
+		});
+		break;
+		
+		case "bar":
+		$.get("barRequest", {day: day}, function(result) {
+			
+		});
+		break;
+		
+		case "line":
+		return;
+	}
+	
 }
 
 $(document).ready(function() {
 	
-	navigators = $(".nav-tabs li");
-	navigators.on("click", function(ev) {
-		$(this).addClass("active");
-		$(navigators.not(this)).removeClass("active");
-		day = this.dataset.day;
-		updateData();
-	});
-
+	svgContainer = document.getElementById("svgContainer");
+	$(window).on("resize", updateSizes);
+	
+	function setEvents(selector, variable) {
+		var group = $(selector + " li");
+		group.on("click", function(ev) {
+			if (this.className.indexOf("active") != -1)
+				return;
+			$(this).addClass("active");
+			$(group.not(this)).removeClass("active");
+			window[variable] = this.dataset[variable];
+			updatePage();
+		});
+	}
+	
+	setEvents(".nav-tabs", "day");
+	setEvents(".nav-pills", "chart");
+	
+	updatePage();
+	
 });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var mysql = require ("mysql");
+var fs = require("fs");
 
 app.use(express.static(__dirname));
 
@@ -33,6 +34,78 @@ var server = app.listen(8080, function(err) {
 			if (err) throw err;
 			console.log("The query successfully returned " + result.length + " rows.");
 			res.json(result);
+		});
+	});
+	
+	//Invio del grafico dei nodi
+	app.get("/nodeRequest", function(req, res) {
+		console.log("Request received for node chart data");
+		console.log("Options:", req.query);
+		
+		fs.readFile("assets/data/nodeData" + req.query.day + ".json", function(err, result) {
+			
+			if (err) throw err;
+			result = JSON.parse(result.toString());
+			console.log("Loaded " + result.nodes.length + " nodes and " + result.links.length + " links.");
+			
+			var values;
+			var minValue;
+			
+			//Individuazione degli archi deboli
+			values = [];
+			for (var i = 0; i < result.links.length; i++) {
+				values.push(result.links[i].value);
+			}
+			values.sort(function(a,b) {
+				return a - b;
+			});
+			minValue = values[values.length - req.query.maxLinks];
+			
+			//Eliminazione degli archi deboli
+			result.links = result.links.filter(function(d) {
+				return d.value > minValue;
+			});
+			
+			//Individuazione dei nodi deboli
+			var values = [];
+			for (var i = 0; i < result.nodes.length; i++) {
+				values.push(result.nodes[i].inValue + result.nodes[i].outValue);
+			}
+			values.sort(function(a,b) {
+				return a - b;
+			});
+			minValue = values[values.length - req.query.maxNodes];	
+			
+			//Eliminazione dei nodi deboli e aggiornamento degli indici degli archi
+			for (var i = 0; i < result.nodes.length; i++) {
+				if (result.nodes[i].inValue + result.nodes[i].outValue < minValue) {
+					result.nodes.splice(i, 1);
+					for (var j = 0; j < result.links.length; j++) {
+						var arco = result.links[j];
+						if (arco.source == i || arco.target == i) {
+							result.links.splice(j, 1)
+							j--;
+							continue;
+						}
+						if (arco.source > i)
+							arco.source--;
+						if (arco.target > i)
+							arco.target--;
+					}
+					i--;
+				}
+			}
+			
+			console.log("Data processed for " + result.nodes.length + " nodes and " + result.links.length + " links.");
+			
+			//Riduzione del valore degli archi per una migliore rappresentazione
+			for (var i = 0; i < result.links.length; i++) {
+				result.links[i].value /= 100;
+			}
+			
+			//Invio dei dati al client
+			res.json(JSON.stringify(result));
+			
 		});
 	});
 	
